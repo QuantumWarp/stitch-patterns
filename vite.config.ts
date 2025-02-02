d8762fc46e0e56c33cb27970960eabbcabd561f0
John Lowther <jw.lowther@hotmail.co.uk>
2025-02-02 15:39:44 +0000

    Update node version

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
+import { fileURLToPath, URL } from 'node:url';
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
-  plugins: [vue()],
+  base: "./",
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
 });
